6650d0b5d963968066d56d1c22f91032c946bac1
Susan56789 <devnimoh@gmail.com>
2023-11-07 13:28:48 +0300

    Scroll To Top

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <ScrollToTopButton />
   </div>
 </template>
 
 <script>
+import ScrollToTopButton from './components/ScrollToTopButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ScrollToTopButton
+  }
+};
 </script>
 
 <style>
---
+++ src/components/ScrollToTopButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-show="isVisible" @click="scrollToTop" class="fixed bottom-6 right-6 cursor-pointer">
+        <i
+            class="fas fa-arrow-up fixed bottom-6 right-6 bg-purple-500 hover:bg-purple-700 text-white py-2 px-4 rounded"></i>
+    </div>
+</template>
+  
+<script>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+export default {
+    name: "ScrollToTopButton",
+    setup() {
+        const isVisible = ref(false);
+
+        const handleScroll = () => {
+            isVisible.value = window.scrollY > 200;
+        };
+
+        const scrollToTop = () => {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        };
+
+        onMounted(() => {
+            window.addEventListener("scroll", handleScroll);
+        });
+
+        onBeforeUnmount(() => {
+            window.removeEventListener("scroll", handleScroll);
+        });
+
+        return {
+            isVisible,
+            scrollToTop,
+        };
+    },
+};
+</script>
+  
+<style scoped>
+/* Add FontAwesome CSS */
+@import "~@fortawesome/fontawesome-free/css/all.css";
+</style>
+  
