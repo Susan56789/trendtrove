33fc1ff9b85b9405a01f8e4e685de4fbece0a48a
Susan56789 <devnimoh@gmail.com>
2023-11-08 13:54:04 +0300

    cart page design

--- src/components/CartPage/index.vue
+++ src/components/CartPage/index.vue
@@ -1,89 +1,78 @@
 <template>
-    <div class="h-screen bg-gray-100 pt-20 bg-yellow-50">
-        <h1 class="mb-10 text-center text-2xl font-bold">Cart Items</h1>
-        <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
-            <div class="rounded-lg md:w-2/3">
-                <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-                    <img src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                        alt="product-image" class="w-full rounded-lg sm:w-40" />
-                    <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                        <div class="mt-5 sm:mt-0">
-                            <h2 class="text-lg font-bold text-gray-900">Nike Air Max 2019</h2>
-                            <p class="mt-1 text-xs text-gray-700">36EU - 4US</p>
-                        </div>
-                        <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                            <div class="flex items-center border-gray-100">
-                                <span
-                                    class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
-                                    - </span>
-                                <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number"
-                                    value="2" min="1" />
-                                <span
-                                    class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
-                                    + </span>
-                            </div>
-                            <div class="flex items-center space-x-4">
-                                <p class="text-sm">259.000 ₭</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                    stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
+    <div class="bg-yellow-50">
+        <div class="pt-32">
+            <h1 class="text-center text-2xl font-bold text-gray-800">Your Cart</h1>
+        </div>
+        <div class="container mx-auto my-8">
+            <div class="flex flex-col md:flex-row">
+                <div class="md:w-2/3 pr-8">
+
+                    <div v-if="cart.length === 0" class="text-center text-gray-500">
+                        Your cart is empty.
+                    </div>
+                    <div v-else>
+                        <div class="flex flex-wrap -mx-4">
+                            <div v-for="(product, index) in cart" :key="index"
+                                class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-4">
+                                <div class="bg-white p-6 rounded shadow">
+                                    <div class="w-16 h-16 bg-gray-300 rounded-full mr-4">
+                                        <img :src="product.image" :alt="product.name" class="w-full h-full object-cover">
+                                    </div>
+                                    <div class="flex-1">
+                                        <h3 class="text-lg font-semibold">{{ product.name }}</h3>
+                                        <p class="text-green-500 font-semibold">${{ product.price }}</p>
+                                    </div>
+                                    <div class="flex justify-between items-center">
+                                        <div class="flex justify-between items-center mt-2">
+                                            <button @click="decreaseQuantity(index)"
+                                                class="px-3 py-1 bg-gray-200 rounded-full">-</button>
+                                            <span class="mx-2">{{ product.quantity }}</span>
+                                            <button @click="increaseQuantity(index)"
+                                                class="px-3 py-1 bg-gray-200 rounded-full">+</button>
+                                        </div>
+                                        <div>
+                                            <span class="font-semibold">Total:</span> ${{ (product.quantity *
+                                                product.price).toFixed(2) }}
+                                        </div>
+                                    </div>
+                                    <button @click="removeProduct(index)"
+                                        class="mt-4 text-red-500 hover:text-red-700">Remove</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-                    <img src="https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80"
-                        alt="product-image" class="w-full rounded-lg sm:w-40" />
-                    <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                        <div class="mt-5 sm:mt-0">
-                            <h2 class="text-lg font-bold text-gray-900">Nike Air Max 2019</h2>
-                            <p class="mt-1 text-xs text-gray-700">36EU - 4US</p>
+                <div class="md:w-1/3">
+                    <div class="bg-white p-6 rounded shadow">
+                        <div v-for="(product, index) in cart" :key="index" class="flex justify-between mb-2">
+                            <span>{{ product.name }} ({{ product.quantity }} x ${{ product.price.toFixed(2) }})</span>
+                            <span>${{ (product.quantity * product.price).toFixed(2) }}</span>
                         </div>
-                        <div class="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                            <div class="flex items-center border-gray-100">
-                                <span
-                                    class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
-                                    - </span>
-                                <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number"
-                                    value="2" min="1" />
-                                <span
-                                    class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
-                                    + </span>
+                        <div class="flex justify-between mb-2">
+                            <span>Shipping Fee</span>
+                            <span>${{ shippingFee.toFixed(2) }}</span>
+                        </div>
+                        <div v-if="cart.length > 0">
+                            <div class="flex justify-between border-t pt-2 mt-4">
+                                <span class="font-semibold">Total</span>
+                                <span class="text-xl">${{ (totalCost + shippingFee).toFixed(2) }}</span>
                             </div>
-                            <div class="flex items-center space-x-4">
-                                <p class="text-sm">259.000 ₭</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                    stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </div>
+                            <button
+                                class="w-full mt-4 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+                                @click="checkout">
+                                Checkout
+                            </button>
+                        </div>
+                        <div v-else>
+                            <a href="/shop"
+                                class="w-full mt-4 bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded">
+                                Shop Now
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- Sub total -->
-            <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-                <div class="mb-2 flex justify-between">
-                    <p class="text-gray-700">Subtotal</p>
-                    <p class="text-gray-700">$129.99</p>
-                </div>
-                <div class="flex justify-between">
-                    <p class="text-gray-700">Shipping</p>
-                    <p class="text-gray-700">$4.99</p>
-                </div>
-                <hr class="my-4" />
-                <div class="flex justify-between">
-                    <p class="text-lg font-bold">Total</p>
-                    <div class="">
-                        <p class="mb-1 text-lg font-bold">$134.98 USD</p>
-                        <p class="text-sm text-gray-700">including VAT</p>
-                    </div>
-                </div>
-                <button type="submit"
-                    class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check
-                    out</button>
-            </div>
+
         </div>
     </div>
 </template>
@@ -91,9 +80,42 @@
 <script>
 
 export default {
-    name: 'CartPage', // Single-word component name
-    // Component logic goes here
+    name: 'CartPage',
+    data() {
+        return {
+            cart: [
+                { name: "Product 1", description: "Description 1", quantity: 1, price: 10, image: "/products/rednikeshoe.png" },
+                { name: "Product 2", description: "Description 2", quantity: 2, price: 15, image: "/products/rednikeshoe.png" },
+                { name: "Product 3", description: "Description 3", quantity: 3, price: 20, image: "/products/rednikeshoe.png" },
+                { name: "Product 4", description: "Description 4", quantity: 4, price: 25, image: "/products/rednikeshoe.png" },
+                { name: "Product 5", description: "Description 5", quantity: 5, price: 30, image: "/products/rednikeshoe.png" }
+                // Add more products as needed
+            ],
+            shippingFee: 5, // Sample shipping fee
+        };
+    },
+    computed: {
+        totalCost() {
+            return this.cart.reduce((total, product) => total + product.quantity * product.price, 0);
+        },
+    },
+    methods: {
+        removeProduct(index) {
+            this.cart.splice(index, 1);
+        },
+        decreaseQuantity(index) {
+            if (this.cart[index].quantity > 1) {
+                this.cart[index].quantity--;
+            }
+        },
+        increaseQuantity(index) {
+            this.cart[index].quantity++;
+        },
+
+    },
 };
+
+
 
 </script>
 <style></style>
