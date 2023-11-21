4b87426f69f7ae0fb33a0df29c92a523ec4e0162
susan56789 <devnimoh@gmail.com>
2023-11-21 20:46:26 +0300

    create db tables

--- src/components/DB/data.sql
+++ src/components/DB/data.sql
@@ -1,9 +1,15 @@
 CREATE DATABASE trendstore;
 USE trendstore;
 
-CREATE TABLE images (
-imageID INT PRIMARY KEY, imageData VARBINARY(255), imageName NVARCHAR(255)); 
-
 CREATE TABLE products (
 prod_ID INT PRIMARY KEY, prod_name VARCHAR(200), cat_name VARCHAR(200), prod_description VARCHAR(300),
-prod_specs VARCHAR(1000), imageID INT, quantity INT, ratingID INT, price VARCHAR(200), discounted VARCHAR(200));
+prod_specs VARCHAR(1000), image BLOB, quantity INT, ratingID INT, price VARCHAR(200), discounted VARCHAR(200));
+
+CREATE TABLE category(
+cat_id INT PRIMARY KEY auto_increment, cat_name VARCHAR(200));
+
+CREATE TABLE orders (
+order_ID INT PRIMARY KEY auto_increment, user_id INT, prod_ID INT, order_day date);
+
+CREATE TABLE new_user (
+user_id INT primary key auto_increment, f_name varchar(200), l_name varchar(200), email varchar(200), order_ID INT); 
