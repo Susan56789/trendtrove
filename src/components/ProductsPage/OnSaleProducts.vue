bef96193db7a6119d822dae2fdc3121d0411825a
Susan Wairimu <devnimoh@gmail.com>
2023-11-29 08:48:42 +0000

    responsive design

--- src/components/ProductsPage/OnSaleProducts.vue
+++ src/components/ProductsPage/OnSaleProducts.vue
@@ -13,7 +13,7 @@
                 class="p-2 border rounded shadow-lg hover:shadow-xl hover:transform hover:scale-105 mb-4"
               >
                 <div class="image-container relative flex items-end overflow-hidden rounded-xl">
-                  <img :src="product.image" :alt="product.name" class="mx-auto w-full h-32 object-cover mb-2" />
+                  <img :src="product.image" :alt="product.name" class="mx-auto w-full h-full object-cover mb-2" />
                   <div class="absolute bottom-2 left-2 inline-flex items-center rounded-lg bg-white p-1 shadow-md">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-yellow-400" viewBox="0 0 20 20"
                          fill="currentColor">
@@ -23,21 +23,21 @@
                     <span class="ml-1 text-sm text-gray-700">{{ product.rating }}</span>
                   </div>
                 </div>
-                <h2 class="text-slate-700 text-sm">{{ product.name }}</h2>
-                <p class="mt-1 text-xs text-slate-400">{{ product.description }}</p>
+                <h2 class="text-slate-700 text-xs">{{ product.name }}</h2>
+                <p class="mt-1 text-xxs text-slate-400">{{ product.description }}</p>
                 <div class="mt-2 flex items-end justify-between">
-                  <p class="text-gray-500 line-through text-xs">KES. {{ formatNumber(product.price) }}</p>
-                  <p class="text-green-500 font-semibold text-xs">KES. {{ formatNumber(product.discountedPrice) }}</p>
+                  <p class="text-gray-500 line-through text-xxs">KES. {{ formatNumber(product.price) }}</p>
+                  <p class="text-green-500 font-semibold text-xxs">KES. {{ formatNumber(product.discountedPrice) }}</p>
                   <div class="flex items-center space-x-1.5 rounded-lg bg-indigo-500 px-2 py-1 text-white duration-100 hover:bg-indigo-600">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                          stroke="currentColor" class="h-3 w-3">
                       <path stroke-linecap="round" stroke-linejoin="round"
                             d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
                     </svg>
-                    <button @click="addToCart" class="text-xs w-full">Add to cart</button>
+                    <button @click="addToCart" class="text-xxs w-full">Add to cart</button>
                   </div>
                   <button @click="addToWishlist"
-                          class="bg-green-500 text-white px-1 py-0.5 rounded-lg text-xs duration-100 hover:bg-green-700">
+                          class="bg-green-500 text-white px-1 py-0.5 rounded-lg text-xxs duration-100 hover:bg-green-700">
                     Add to Wishlist
                   </button>
                 </div>
@@ -48,6 +48,7 @@
       </div>
     </div>
   </template>
+  
   
   
 <script>
