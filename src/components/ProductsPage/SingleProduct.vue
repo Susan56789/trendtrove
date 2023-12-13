f1df9ae77ee028e032082a906c5042b0f52a231f
Susan Wairimu <devnimoh@gmail.com>
2023-12-13 12:13:51 +0000

    updates

--- src/components/ProductsPage/SingleProduct.vue
+++ src/components/ProductsPage/SingleProduct.vue
@@ -5,24 +5,30 @@
         <div class="image-container relative flex items-end overflow-hidden rounded-xl">
           <img :src="product.image" :alt="product.name" class="mx-auto h-200 w-200 object-cover mb-4" />
           <div class="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" class="h-3 w-3">
-                      <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
-                    <span class="ml-1 text-sm text-gray-700">{{ product.rating }}</span>
-                    </svg>
-                    <button @click="addToCart" class="text-xxs w-full">Add to cart</button>
-                     <button @click="addToWishlist"
-                          class="bg-green-500 text-white px-1 py-0.5 rounded-lg text-xxs duration-100 hover:bg-green-700">
-                    Add to Wishlist
-                  </button>
-            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+              <!-- Rating star SVG here -->
+            </svg>
+            <span class="ml-1 text-sm text-gray-700">{{ product.rating }}</span>
           </div>
         </div>
         <h2 class="text-slate-700">{{ product.name }}</h2>
         <p class="mt-1 text-sm text-slate-400">{{ product.description }}</p>
         <div class="mt-3 flex items-end justify-between">
-          <!-- Price and Add to Cart button here -->
+          <p v-if="product.discountedPrice" class="text-gray-500 line-through">KES. {{ formatNumber(product.price) }}</p>
+          <p v-if="product.discountedPrice" class="text-green-500 font-semibold">KES. {{ formatNumber(product.discountedPrice) }}</p>
+          <p v-else class="text-green-500 font-semibold">KES. {{ formatNumber(product.price) }}</p>
+
+          <div class="flex items-center space-x-1.5 rounded-lg bg-indigo-500 px-4 py-1.5 text-white duration-100 hover:bg-indigo-600">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+              class="h-4 w-4">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+            <button @click="addToCart" class="text-sm">Add to Cart</button>
+          </div>
+          <button @click="addToWishlist" class="bg-green-500 text-white px-2 py-1.5 rounded-lg duration-100 hover:bg-green-700">
+            Add to Wishlist
+          </button>
         </div>
       </div>
     </article>
@@ -38,8 +44,14 @@ export default {
     },
   },
   methods: {
+    formatNumber(value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
     addToCart() {
       this.$emit('addToCart', this.product);
+    },
+    addToWishlist() {
+      this.$emit('addToWishlist', this.product);
     },
   },
 };
