50e70ba2d806260a6a6dee94f79af2283b588c40
Susan Wairimu <devnimoh@gmail.com>
2023-12-13 09:12:38 +0000

    updated

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [], // Set this to an array of dependencies to transpile, or leave it as an empty array if none need transpilation
   configureWebpack: {
     devtool: 'source-map'
   }
-  
 })
