a966d8bc38d2705cf0b82a92f0aa9c4ca356597a
susan56789 <devnimoh@gmail.com>
2024-01-02 01:11:39 +0300

    added icons

--- src/components/ProductsPage/OnSaleProducts.vue
+++ src/components/ProductsPage/OnSaleProducts.vue
@@ -17,11 +17,7 @@
                 <img :src="product.ImagePath" :alt="product.ProductName"
                   class="mx-auto w-full h-full object-cover mb-2" />
                 <div class="absolute bottom-2 left-2 inline-flex items-center rounded-lg bg-white p-1 shadow-md">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-yellow-400" viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path
-                      d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
+                  <i class="fas fa-star text-yellow-400"></i>
                   <span class="ml-1 text-sm text-gray-700">{{ product.Rating }}</span>
                 </div>
               </div>
@@ -31,17 +27,13 @@
                 <p class="text-green-500 font-semibold text-xxs">KES. {{ formatNumber(product.DiscountedPrice) }}</p>
                 <div
                   class="flex items-center space-x-1.5 rounded-lg bg-indigo-500 px-2 py-1 text-white duration-100 hover:bg-indigo-600">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="h-3 w-3">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                  </svg>
-                  <button @click="addToCartButton(product)" class="text-sm"> Add to Cart</button>
+
+                  <button @click="addToCartButton(product)" class="text-sm"> <i class="fas fa-cart-plus"></i></button>
 
                 </div>
                 <button @click="addToWishlistButton(product)"
                   class="bg-green-500 text-white px-2 py-1.5 rounded-lg duration-100 hover:bg-green-700">
-                  Add to Wishlist
+                  <i class="fas fa-heart"></i>
                 </button>
 
               </div>
