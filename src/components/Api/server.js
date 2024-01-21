e3ffd80cd8975f67edda2fa077947df699764b44
susan56789 <devnimoh@gmail.com>
2024-01-21 16:15:54 +0300

    connect to local db

--- src/components/Api/server.js
+++ src/components/Api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -25,25 +25,24 @@ app.use((req, res, next) => {
 });
 
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 3306,
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'trendstore',
+
     waitForConnections: true,
     connectionLimit: 30000,
     queueLimit: 0
 });
 
-connection.getConnection((err, connection) => {
+connection.connect((err) => {
     if (err) {
         console.error('Database connection failed: ', err);
         return;
     }
     console.log('Connected to MySQL database');
-    // Don't forget to release the connection after using it
-    connection.release();
+
 });
 
 
